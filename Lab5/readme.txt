8b5bc826f64188258de4beb24e65c0fa045f5fab
Tristan Villamil <villamil101@gmail.com>
2016-03-09 23:12:10 -0500

    Lab 5
    Finished with Lab 5

---
+++ Lab5/readme.txt
@@ -0,0 +1,9 @@
+I used the Twit module to read in the twitter api because it was the most intuitive to use in my opinion.
+
+I used fs to output the information into a JSON file which can be used as input for lab 1 (tweets.json).
+
+The server was created using express, where the API was accessed.
+
+Once accessed, the server passed the information back using socket.emit (the same way the query was passed to the server)
+
+I experimented with vw in CSS, in an attempt to make the button more dynamically-sized when the page shrinks. I don't like it as much as I thought I would, I will continue experimenting.
--- Lab5/server.js
+++ Lab5/server.js
@@ -3,22 +3,13 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var mongoose = require('mongoose');
+var fs = require('fs');
 
 app.use(express.static('public'));
 
 //added for twitter API
-var twitterAPI = require('node-tweet-stream');
-var twitter = new twitterAPI({
-  consumer_key: 'TCaOuiccF049bY9okramtr2p9',
-  consumer_secret: 'Qzb9pHez2U3zYiLuB5AWpRzE6WlutcO2K2zuPkyL0wJvztO6FH',
-  token: '1068174990-wgmbWXIxcmwsL4RM0ZFjLZ8bcdp6ezRs0bt9Cw1',
-  token_secret: 'rxplJPs24nn5ItqkSzq28JUHZibMTdnjeNE1G9aiRRdt1'
-});
-
-//added for twitter API
-var twitAPI = require('twitter');
-var twit = new twitterAPI({
+var TwitterAPI = require('twit');
+var Twitter = TwitterAPI({
   consumer_key: 'TCaOuiccF049bY9okramtr2p9',
   consumer_secret: 'Qzb9pHez2U3zYiLuB5AWpRzE6WlutcO2K2zuPkyL0wJvztO6FH',
   access_token: '1068174990-wgmbWXIxcmwsL4RM0ZFjLZ8bcdp6ezRs0bt9Cw1',
@@ -30,23 +21,8 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-// connect to mongodb
-var db = mongoose.connection;
-db.on('error', console.error);
-mongoose.connect('mongodb://localhost/mychat');
-
-// mongodb schemas
-var chatMessage = new mongoose.Schema({
-  username: String,
-  message: String
-});
-var Message = mongoose.model('Message', chatMessage);
-
 // user connected even handler
 io.on('connection', function(socket){
-  var tracking = "";
-  var max = 0;
-  var count = 0;
   
   // log & brodcast connect event
   console.log('a user connected');
@@ -55,52 +31,51 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-
-  //turns on stream
-  twitter.on('tweet', function (tweet) {
-    ++count;
-//    console.log('tweet received', tweet);
-    socket.emit('tweet', tweet);
-    if(max <= count){
-      twitter.untrack(tracking);
-      count = 0;
-    }
-  });
-   
-  twitter.on('error', function (err) {
-    console.log('Oh no')
-  });
   
   // message received event handler
   socket.on('query', function(query){
-    twit.stream('statuses/filter', {track: 'Trump'}, function(stream) {
-  stream.on('data', function(tweet) {
-    console.log("tweet.text");
-  });
- 
-  stream.on('error', function(error) {
-    throw error;
-  });
-});
 
-    count = 0;
-    max = query[1];
-    twitter.untrack(tracking);
-    tracking = query[0];
+    //variables
+    var count = 0;
+    var max = query[1];
+    var tracking = query[0];
+    var Troy = [ '-73.68', '42.72', '-73.67', '42.73' ];
+    if(query[0] != ''){ var stream = Twitter.stream('statuses/filter', {track: query[0]}); }
+    else{ var stream = Twitter.stream('statuses/filter', {locations: Troy}); }
 
-    twitter.track(query[0]);
-    // log chat msg
-//    console.log('query: ' + query[0]);
-    
-    // save message to db
-    var message = new Message ({
-      message : query[0]
-    });
-    message.save(function (err, saved) {
-      if (err) {
-	      return console.log('error saving to db');
+    fs.writeFile(__dirname + "/tweets.json", "[", function(err) {if(err) { return console.log(err); } });
+
+    //turns on stream
+    var bool = stream.on('tweet', function (tweet) {
+      //when we have all of the required tweets, return true for later
+      if(max <= count){ return true; }
+
+      //send tweet back to client
+      socket.emit('tweet', tweet);
+
+      //increment count
+      ++count;
+
+      //for outputting to file, see if we need a comma between items in the array
+      if(count != max){
+        fs.appendFile(__dirname + "/tweets.json", JSON.stringify(tweet) + ", ", function(err) {if(err) { return console.log(err); } });
+      }else{
+        fs.appendFile(__dirname + "/tweets.json", JSON.stringify(tweet) + "]", function(err) {if(err) { return console.log(err); } });
       }
-    })
+      return false;
+    });
+     
+    stream.on('error', function (err) {
+      console.log('Oh no')
+    });
+
+    // log query
+    console.log('query: ' + query[0]);
+    
+    //close out this function if we have all required tweets
+    if(bool){
+      return false;
+    }
   });
 });
 
